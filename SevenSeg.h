25dedbae18f712eb8ee7cc6aa8d1fa01a45e0b90
Michael Brunner <MichaelBrunn3r@gmail.com>
2018-04-09 16:37:44 +0200

    Added some description to "SevenSeg.h"

--- SevenSeg.h
+++ SevenSeg.h
@@ -1,6 +1,20 @@
-#ifndef Segments_HEADER
-#define Segments_HEADER
+#ifndef SevenSeg_HEADER
+#define SevenSeg_HEADER
 
+/**
+ * Segments of a Seven Segment Display
+ *  aaa
+ * f   b
+ * f   b
+ *  ggg
+ * e   c
+ * e   c
+ *  ddd   hh
+ **/
+
+/**
+ * Binary codes for each Segment, so a state can be represented in one byte.
+ **/
 enum Segment {
     a=1, b=2, c=4, d=8, e=16, f=32, g=64, h=128
 };
